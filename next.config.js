e2b9c6e69a5d8cf340e5ab8344fdd29296bbb803
abashir7866 <79613256+abashir7866@users.noreply.github.com>
2022-05-16 12:32:49 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,30 +1,30 @@
 module.exports = {
   env: {
-    userName: "NahSahh",
+    userName: "abashir7866",
     cards: [
       {
-        title: "Kythi",
-        link: "https://discord.gg/Fa7t67NEMU",
-        role: "Owner",
-        purpose: "Public File-sharing",
+        title: "uhh idfk",
+        link: "balls",
+        role: "professional retard",
+        purpose: "i serve no purpose",
         paragraph:
-          "Kythi is a public free-to-use file-uploader that allows you to upload images and share them with anyone, along with customized embeds to make them look like you want.",
+          "please send me money em poor",
       },
       {
-        title: "Kraken",
-        link: "https://kraken.pics",
-        role: "Developer",
-        purpose: "Public File Uploader",
+        title: "a",
+        link: "a",
+        role: "a",
+        purpose: "a",
         paragraph:
-          "Kraken is a public file-uploader, used to store files and customise embeds, with the choice of custom domains, allowing you to personalise your images your way.",
+          "a",
       },
       {
-        title: "E-Z.host",
-        link: "https://e-z.host",
-        role: "Contributor",
-        purpose: "Private File Uploader",
+        title: "a",
+        link: "a",
+        role: "a",
+        purpose: "a",
         paragraph:
-          "E-Z.host is a closed-private file-uploader, used to store uploads and customise embeds, it has a plethora of features to choose from allowing you to personalise your files, you can purchase it on their website.",
+          "send help",
       },
     ],
   },
